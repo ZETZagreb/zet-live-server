ccf9c988cfaf552589174261bea1ebf88a1ae359
ZETZagreb <maric.leon2001@gmail.com>
2026-04-21 21:15:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/split-data')
+def get_split_data():
+    url = "https://fleet.promet-split.hr/api/get-positions"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    }
+    try:
+        # Render server šalje upit, što Split ne blokira
+        response = requests.get(url, headers=headers, timeout=10)
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
